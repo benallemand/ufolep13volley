8f7d383c9df12fdb4331a536b78addcc8810a202
Ben <Ben@Ben-PC>
2013-10-04 21:26:33 +0200

    /** Correction des accents */

--- portail.php
+++ portail.php
@@ -26,7 +26,7 @@
 			<div id="portail">
 			
 <?php 
-// on traite si la variable log=err est d?tect?e
+// on traite si la variable log=err est détectée
 $err = "";
 if (isset($_GET['log'])) 
   {
@@ -34,7 +34,7 @@ if (isset($_GET['log']))
   elseif ($_GET=="noauth") {$err = "Echec d'authentification - Login ou mot de passe incorrect !";}
   else {$err = "Echec d'authentification - Veuillez réessayer";}
   }
-// on traite si les sessions $_SESSION sont cr??es
+// on traite si les sessions $_SESSION sont créées
 if (isset($_SESSION['login']) && isset($_SESSION['password'])) 
 	{
 	affich_portail_equipe($_SESSION['login']);
