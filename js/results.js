20992231185b6ced8db14e3429ee41ff0bfd7b35
Ben <Ben@Ben-PC>
2013-10-07 22:45:18 +0200

    /** AutoWidth des colonnes */

--- js/results.js
+++ js/results.js
@@ -1,4 +1,6 @@
 Ext.onReady(function() {
+    Ext.Loader.setPath('Ext.ux', 'js/libs');
+    Ext.require('Ext.ux.ColumnAutoWidthPlugin');
     Ext.create('Ext.Button', {
         text: 'Derniers résultats...',
         margin: 10,
@@ -6,54 +8,63 @@ Ext.onReady(function() {
         handler: function() {
             Ext.create('Ext.window.Window', {
                 title: 'Derniers résultats',
+                maximizable: true,
                 height: 400,
                 width: 900,
                 layout: 'fit',
                 items: {
                     xtype: 'grid',
-                    border: false,
-                    columns: [
-                        {
-                            header: 'competition',
-                            dataIndex: 'competition'
-                        },
-                        {
-                            header: 'division',
-                            dataIndex: 'division'
-                        },
-                        {
-                            header: 'equipe_domicile',
-                            dataIndex: 'equipe_domicile'
-                        },
-                        {
-                            header: 'equipe_exterieur',
-                            dataIndex: 'equipe_exterieur'
-                        },
-                        {
-                            header: 'set1',
-                            dataIndex: 'set1'
-                        },
-                        {
-                            header: 'set2',
-                            dataIndex: 'set2'
-                        },
-                        {
-                            header: 'set3',
-                            dataIndex: 'set3'
-                        },
-                        {
-                            header: 'set4',
-                            dataIndex: 'set4'
-                        },
-                        {
-                            header: 'set5',
-                            dataIndex: 'set5'
-                        },
-                        {
-                            header: 'date_reception',
-                            dataIndex: 'date_reception'
-                        }
+                    forceFit: true,
+                    plugins: [
+                        Ext.create('Ext.ux.ColumnAutoWidthPlugin', {})
                     ],
+                    columns: {
+                        items: [
+                            {
+                                header: 'competition',
+                                dataIndex: 'competition'
+                            },
+                            {
+                                header: 'division',
+                                dataIndex: 'division'
+                            },
+                            {
+                                header: 'equipe_domicile',
+                                dataIndex: 'equipe_domicile'
+                            },
+                            {
+                                header: 'equipe_exterieur',
+                                dataIndex: 'equipe_exterieur'
+                            },
+                            {
+                                header: 'set1',
+                                dataIndex: 'set1'
+                            },
+                            {
+                                header: 'set2',
+                                dataIndex: 'set2'
+                            },
+                            {
+                                header: 'set3',
+                                dataIndex: 'set3'
+                            },
+                            {
+                                header: 'set4',
+                                dataIndex: 'set4'
+                            },
+                            {
+                                header: 'set5',
+                                dataIndex: 'set5'
+                            },
+                            {
+                                header: 'date_reception',
+                                dataIndex: 'date_reception'
+                            }
+                        ],
+                        defaults: {
+                            autoWidth: true
+                        }
+                    },
                     store: Ext.create('Ext.data.Store', {
                         fields: [
                             'competition',
