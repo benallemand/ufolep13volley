3192720fdee63d513c6db9b52e6e08daf3e246e6
Ben <Ben@Ben-PC>
2014-03-09 14:46:13 +0100

    Poule au lieu de Division dans l'annuaire

--- js/annuaire.js
+++ js/annuaire.js
@@ -74,14 +74,18 @@ Ext.onReady(function() {
                                                 storeGrid.filter('libelle_competition', competition);
                                                 storeGrid.filter('division', division);
                                                 storeGrid.sort('nom_equipe', 'ASC');
+                                                var libelle_division = 'Division ';
+                                                if (competition.indexOf('Coupe') !== -1) {
+                                                    libelle_division = 'Poule ';
+                                                }
                                                 panelDivision.add({
                                                     xtype: 'grid',
                                                     store: storeGrid,
                                                     columns: [
                                                         {
-                                                            header : 'Division ' + division,
+                                                            header: libelle_division + division,
                                                             dataIndex: 'nom_equipe',
-                                                            flex : 1,
+                                                            flex: 1,
                                                             renderer: function(value, meta, record) {
                                                                 return Ext.String.format("<a href='annuaire.php?id={0}&c={1}' target='_self'>{2}</a>", record.get('id_equipe'), record.get('code_competition'), record.get('nom_equipe'));
                                                             }
