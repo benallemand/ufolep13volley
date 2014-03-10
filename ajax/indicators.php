29120ba55d8c9a1fa3b633203d57df5db0eb0f81
Ben <Ben@Ben-PC>
2014-03-10 20:03:21 +0100

    Indicateur de matches dupliqués

--- ajax/indicators.php
+++ ajax/indicators.php
@@ -1,6 +1,12 @@
 <?php
 
 require_once 'classes/Indicator.php';
+$indicatorMatchesDupliques = new Indicator(
+        'Match dupliqué', "SELECT e1.nom_equipe, e2.nom_equipe, m.code_match, COUNT(*) FROM matches m
+        JOIN equipes e1 ON e1.id_equipe = m.id_equipe_dom
+        JOIN equipes e2 ON e2.id_equipe = m.id_equipe_ext
+        GROUP BY m.id_equipe_dom, m.id_equipe_ext, m.code_competition
+        HAVING COUNT(*) > 1");
 $indicatorEquipesSansClub = new Indicator(
         'Club non renseigné', "SELECT e.nom_equipe,
         comp.libelle,
@@ -70,4 +76,5 @@ $results[] = $indicatorMatchesNonRenseignes->getResult();
 $results[] = $indicatorEquipesEngageesChampionnat->getResult();
 $results[] = $indicatorInfosManquantes->getResult();
 $results[] = $indicatorEquipesSansClub->getResult();
+$results[] = $indicatorMatchesDupliques->getResult();
 echo json_encode(array('results' => $results));
