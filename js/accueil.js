719997dd8865848e094cd6088d182934df9c2493
Ben <Ben@Ben-PC>
2014-05-15 13:48:37 +0200

    cancel display color for each competition

--- js/accueil.js
+++ js/accueil.js
@@ -249,28 +249,7 @@ Ext.onReady(function() {
                     {
                         header: 'Compétition',
                         dataIndex: 'competition',
-                        width: 180,
-                        renderer: function(val, meta, record) {
-                            switch (record.get('code_competition')) {
-                                case 'm':
-                                    meta.tdAttr = 'style="background-color:lightblue;color:black;"';
-                                    break;
-                                case 'f':
-                                    meta.tdAttr = 'style="background-color:pink;color:black;"';
-                                    break;
-                                case 'c':
-                                case 'cf':
-                                    meta.tdAttr = 'style="background-color:yellow;color:black;"';
-                                    break;
-                                case 'kh':
-                                case 'kf':
-                                    meta.tdAttr = 'style="background-color:orange;color:black;"';
-                                    break;
-                                default :
-                                    break;
-                            }
-                            return val;
-                        }
+                        width: 180
                     },
                     {
                         header: 'Journée',
