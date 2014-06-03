01fd93f95eb108bd7f3dba638d1a2e1134a30b14
Ben <Ben@Ben-PC>
2014-06-03 22:17:05 +0200

    use of unirest instead of curl

--- ajax/getVolleyballImages.php
+++ ajax/getVolleyballImages.php
@@ -1,20 +1,7 @@
 <?php
-function get_data($url) {
-	$ch = curl_init();
-	$timeout = 5;
-	curl_setopt($ch, CURLOPT_URL, $url);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-	curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-	$data = curl_exec($ch);
-        if($data === false) {
-            echo curl_error($ch);
-        }
-	curl_close($ch);
-	return $data;
-}
 require_once '../includes/db_inc.php';
-$results = array();
-
+require_once '../libs/Unirest.php';
 $url = "https://api.flickr.com/services/rest?method=flickr.photos.search&sort=relevance&api_key=$flickr_api_key&text=volleyball&format=json&extras=url_c&nojsoncallback=1";
-echo get_data($url);
+Unirest::verifyPeer(false);
+$response = Unirest::get($url);
+echo $response->__get('raw_body');
