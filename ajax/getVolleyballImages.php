6f4abd4a134f09e064596aeb85283ab7d3941e20
Ben <Ben@Ben-PC>
2014-06-20 20:12:02 +0200

    revert : modify volleyball images to flicker's last posted ones

--- ajax/getVolleyballImages.php
+++ ajax/getVolleyballImages.php
@@ -1,7 +1,7 @@
 <?php
 require_once '../includes/db_inc.php';
 require_once '../libs/Unirest.php';
-$url = "https://api.flickr.com/services/rest?method=flickr.photos.search&sort=date-posted-desc&api_key=$flickr_api_key&text=volleyball&format=json&extras=url_c&nojsoncallback=1";
+$url = "https://api.flickr.com/services/rest?method=flickr.photos.search&sort=relevance&api_key=$flickr_api_key&text=volleyball&format=json&extras=url_c&nojsoncallback=1";
 Unirest::verifyPeer(false);
 $response = Unirest::get($url);
 echo $response->__get('raw_body');
