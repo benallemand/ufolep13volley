909a9b8abc647ab3c274097466ffa7f03da16e57
Ben <Ben@Ben-PC>
2014-06-21 09:43:10 +0200

    Creat table to manage users profiles

---
+++ nbproject/sql/005-Add table users_profiles.sql
@@ -0,0 +1,5 @@
+CREATE TABLE users_profiles (
+    id smallint(10) PRIMARY KEY AUTO_INCREMENT,
+    user_id smallint(10) NOT NULL,
+    profile_id smallint(10) NOT NULL
+);
---
+++ nbproject/sql/006-Add table profiles.sql
@@ -0,0 +1,5 @@
+CREATE TABLE profiles (
+    id smallint(10) PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(200) NOT NULL
+);
+
