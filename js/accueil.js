4343de0c88779feff5807834e7945e3c77179429
Ben <Ben@Ben-PC>
2014-08-26 22:00:36 +0200

    remove news functionnality

--- js/accueil.js
+++ js/accueil.js
@@ -1,9 +1,9 @@
 Ext.application({
     requires: [],
-    views: ['site.Banner', 'site.MainMenu', 'site.MainPanel', 'site.HeaderPanel', 'site.TitlePanel', 'new.Grid', 'match.LastResultsGrid', 'forum.LastPostsGrid', 'team.WebSitesGrid'],
+    views: ['site.Banner', 'site.MainMenu', 'site.MainPanel', 'site.HeaderPanel', 'site.TitlePanel', 'match.LastResultsGrid', 'forum.LastPostsGrid', 'team.WebSitesGrid'],
     controllers: [],
-    stores: ['News', 'LastResults', 'LastPosts', 'WebSites'],
-    models: ['New', 'LastResult', 'LastPost', 'WebSite'],
+    stores: ['LastResults', 'LastPosts', 'WebSites'],
+    models: ['LastResult', 'LastPost', 'WebSite'],
     name: 'Ufolep13Volley',
     appFolder: 'js',
     launch: function() {
