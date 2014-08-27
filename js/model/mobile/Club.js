155f96c0e67825a92ee334ef5df34628937ebc46
Ben <Ben@Ben-PC>
2014-08-27 21:45:55 +0200

    obsolete files

--- js/model/mobile/Club.js
+++
@@ -1,9 +0,0 @@
-Ext.define('Ufolep13Volley.model.mobile.Club', {
-    extend: 'Ext.data.Model',
-    config: {
-        fields: [
-            {name: 'id', type: 'int'},
-            'nom'
-        ]
-    }
-});
