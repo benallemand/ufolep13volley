9e811a68a84a1d16e0a9041fc10e1e44ef944447
Ben <Ben@Ben-PC>
2014-09-22 22:06:09 +0200

    Add profile in accounts indicator

--- ajax/indicators.php
+++ ajax/indicators.php
@@ -57,9 +57,12 @@ $indicatorComptes = new Indicator(
         'Comptes', "SELECT 
             e.nom_equipe, 
             ca.email, 
-            ca.login 
+            ca.login,
+            p.name AS profil
             FROM equipes e
             JOIN comptes_acces ca ON ca.id_equipe=e.id_equipe
+            JOIN users_profiles up ON up.user_id = ca.id
+            JOIN profiles p ON p.id = up.profile_id
             ORDER BY e.nom_equipe");
 $indicatorMatchesDupliques = new Indicator(
         'Matches dupliqués', "SELECT e1.nom_equipe, e2.nom_equipe, m.code_match, COUNT(*) FROM matches m
