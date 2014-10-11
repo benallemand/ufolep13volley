9e8bd8ab63b49df18edaa025adb839bbc61b2164
Ben <Ben@Ben-PC>
2014-10-11 23:06:50 +0200

    update male points for new season

---
+++ nbproject/sql/020-clear male points for new season.sql
@@ -0,0 +1,15 @@
+UPDATE classements SET 
+points=0,
+joues=0,
+gagnes=0,
+perdus=0,
+sets_pour=0,
+sets_contre=0,
+difference=0,
+coeff_sets=0,
+points_pour=0,
+points_contre=0,
+coeff_points=0,
+penalite=0
+WHERE code_competition = 'm';
+
