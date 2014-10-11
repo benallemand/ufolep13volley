2a1894256ca5b46b0b85c7bd6aff95c99b1c4ab7
Ben <Ben@Ben-PC>
2014-10-11 23:49:38 +0200

    delete unused teams

--- nbproject/sql/021-delete unused teams.sql
+++ nbproject/sql/021-delete unused teams.sql
@@ -2,3 +2,6 @@ DELETE FROM equipes
 WHERE 
 (id_equipe NOT IN (SELECT c.id_equipe FROM classements c)
 AND id_equipe NOT IN (156, 198, 199, 201, 202, 203));
+
+DELETE FROM details_equipes 
+WHERE id_equipe NOT IN (SELECT e.id_equipe FROM equipes e);
