9e3c742af16ceb3b2cc0bd5ef0815e8d32353b51
Ben <Ben@Ben-PC>
2014-11-11 23:57:18 +0100

    New indicator: players without licence number

--- ajax/indicators.php
+++ ajax/indicators.php
@@ -28,6 +28,21 @@ function generateCsv($data, $delimiter = ',', $enclosure = '"') {
 }
 
 require_once 'classes/Indicator.php';
+$indicatorPlayersWithoutLicence = new Indicator(
+        'Joueurs sans numéro de licence', "SELECT 
+        CONCAT(j.nom, ' ', j.prenom) AS joueur, 
+        c.nom AS club,
+        CONCAT(e.nom_equipe, ' (', comp.libelle, ')') AS equipe,
+        jresp.email AS responsable
+        FROM joueur_equipe je 
+        JOIN joueurs j ON j.id = je.id_joueur
+        JOIN equipes e ON e.id_equipe = je.id_equipe
+        JOIN joueur_equipe jeresp ON jeresp.id_equipe = e.id_equipe AND jeresp.is_leader+0 > 0
+        JOIN joueurs jresp ON jresp.id = jeresp.id_joueur
+        JOIN competitions comp ON comp.code_competition = e.code_competition
+        JOIN clubs c ON c.id = j.id_club
+        WHERE j.num_licence = ''
+        ORDER BY equipe ASC");
 $indicatorDoublonsJoueursEquipes = new Indicator(
         'Doublons dans une équipe', "SELECT id_joueur, id_equipe, COUNT(*) AS cnt FROM joueur_equipe
         GROUP BY id_joueur, id_equipe
@@ -161,6 +176,7 @@ $results[] = $indicatorEquipesSansClub->getResult();
 $results[] = $indicatorMatchesDupliques->getResult();
 $results[] = $indicatorComptes->getResult();
 $results[] = $indicatorDoublonsJoueursEquipes->getResult();
+$results[] = $indicatorPlayersWithoutLicence->getResult();
 $indicatorName = utf8_decode(filter_input(INPUT_GET, 'indicator'));
 if (!$indicatorName) {
     echo json_encode(utf8_encode_mix(array('results' => $results)));
