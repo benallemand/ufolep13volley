c19e94053784cd02a865c48923cb466fbd792922
unknown <ballemand@Inside1696.insidefr.com>
2016-02-26 16:52:45 +0100

    add foreign key for timeslot to matches

---
+++ nbproject/sql/042-alter table matches add creneau foreign key.sql
@@ -0,0 +1,20 @@
+-- TODO run in PROD
+SET FOREIGN_KEY_CHECKS = 0;
+ALTER TABLE matches DROP FOREIGN KEY fk_matches_creneau;
+ALTER TABLE matches DROP COLUMN id_creneau;
+ALTER TABLE matches ADD COLUMN id_creneau SMALLINT(10) NOT NULL;
+ALTER TABLE matches ADD CONSTRAINT fk_matches_creneau FOREIGN KEY (id_creneau) REFERENCES creneau (id);
+UPDATE matches m
+  JOIN creneau cr ON
+                    cr.id_equipe = m.id_equipe_dom AND
+                    cr.jour = ELT(WEEKDAY(m.date_reception) + 2,
+                                  'Dimanche',
+                                  'Lundi',
+                                  'Mardi',
+                                  'Mercredi',
+                                  'Jeudi',
+                                  'Vendredi',
+                                  'Samedi')
+SET m.id_creneau = cr.id, m.heure_reception = '';
+SET FOREIGN_KEY_CHECKS = 1;
+-- ALTER TABLE matches DROP COLUMN heure_reception;
