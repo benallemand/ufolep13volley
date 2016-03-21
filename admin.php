5d9d1865605b4ddd4e2360f07b3eebcfd4290867
benallemand <benallemand@gmail.com>
2016-03-21 14:08:03 +0100

    refactor admin page to be similar to other pages

--- admin.php
+++ admin.php
@@ -5,20 +5,22 @@ if (!isAdmin()) {
 }
 ?>
 
-<!DOCTYPE HTML>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
 
 <HTML>
-    <HEAD>
-        <TITLE>Administration UFOLEP 13 VOLLEY</TITLE>
-        <META http-equiv="Content-Type" content="text/html; charset=utf-8" />
-        <link rel="shortcut icon" href="favicon.ico" />
-        <LINK href="includes/main.css" rel="stylesheet" type="text/css" media="screen" />
-        <link href="https://cdn.sencha.com/ext/gpl/5.1.0/build/packages/ext-theme-neptune/build/resources/ext-theme-neptune-all-debug.css" rel="stylesheet" />
-        <script src="https://cdn.sencha.com/ext/gpl/5.1.0/build/ext-all.js"></script>
-        <script src="https://cdn.sencha.com/ext/gpl/5.1.0/build/packages/ext-locale/build/ext-locale-fr.js"></script>
-        <script type="text/javascript" src="js/libs/Commons.js"></script>
-        <script type="text/javascript" src="js/administration.js"></script>
-    </HEAD>
-    <BODY>
-    </BODY>
+<HEAD>
+    <TITLE>Administration UFOLEP 13 VOLLEY</TITLE>
+    <META http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <link rel="shortcut icon" href="favicon.ico"/>
+    <link
+        href="https://cdn.sencha.com/ext/gpl/5.1.0/build/packages/ext-theme-neptune/build/resources/ext-theme-neptune-all-debug.css"
+        rel="stylesheet"/>
+    <script src="https://cdn.sencha.com/ext/gpl/5.1.0/build/ext-all.js" type="text/javascript"></script>
+    <script src="https://cdn.sencha.com/ext/gpl/5.1.0/build/packages/ext-locale/build/ext-locale-fr.js"
+            type="text/javascript"></script>
+    <script type="text/javascript" src="js/libs/Commons.js"></script>
+    <script type="text/javascript" src="js/administration.js"></script>
+</HEAD>
+<BODY>
+</BODY>
 </HTML>
