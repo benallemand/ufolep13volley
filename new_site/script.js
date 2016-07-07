fc8c834dd34f3c9944688d7e58ed0eb7700573a5
benallemand <benallemand@gmail.com>
2016-07-07 15:06:53 +0200

    new site tests

--- new_site/script.js
+++ new_site/script.js
@@ -1,5 +1,5 @@
 // create the module and name it scotchApp
-var scotchApp = angular.module('scotchApp', ['ngRoute']);
+var scotchApp = angular.module('scotchApp', ['ngRoute', 'ngAnimate', 'ui.bootstrap']);
 
 // configure our routes
 scotchApp.config(function ($routeProvider) {
@@ -75,6 +75,17 @@ scotchApp.controller('gymnasiumsController', function ($scope, $http) {
         });
 });
 
+scotchApp.controller('volleyballImagesController', function ($scope, $http) {
+    $http.get("../ajax/getVolleyballImages.php")
+        .then(function (response) {
+            $scope.volleyballImages = [];
+            for(var i=0;i<20;i++) {
+                response.data.photo[i]["index"] = i;
+                $scope.volleyballImages.push(response.data.photo[i]);
+            }
+        });
+});
+
 scotchApp.filter('sanitize', ['$sce', function ($sce) {
     return function (htmlCode) {
         return $sce.trustAsHtml(htmlCode);
@@ -86,3 +97,58 @@ scotchApp.filter('parseDate', function () {
         return new Date(input);
     };
 });
+
+// scotchApp.controller('CarouselDemoCtrl', function ($scope) {
+//     var slides = $scope.slides = [];
+//     var currIndex = 0;
+//
+//     $scope.addSlide = function () {
+//         var newWidth = 600 + slides.length + 1;
+//         slides.push({
+//             image: 'http://lorempixel.com/' + newWidth + '/300',
+//             text: ['Nice image', 'Awesome photograph', 'That is so cool', 'I love that'][slides.length % 4],
+//             id: currIndex++
+//         });
+//     };
+//
+//     $scope.randomize = function () {
+//         var indexes = generateIndexesArray();
+//         assignNewIndexesToSlides(indexes);
+//     };
+//
+//     for (var i = 0; i < 4; i++) {
+//         $scope.addSlide();
+//     }
+//
+//     // Randomize logic below
+//
+//     function assignNewIndexesToSlides(indexes) {
+//         for (var i = 0, l = slides.length; i < l; i++) {
+//             slides[i].id = indexes.pop();
+//         }
+//     }
+//
+//     function generateIndexesArray() {
+//         var indexes = [];
+//         for (var i = 0; i < currIndex; ++i) {
+//             indexes[i] = i;
+//         }
+//         return shuffle(indexes);
+//     }
+//
+//     // http://stackoverflow.com/questions/962802#962890
+//     function shuffle(array) {
+//         var tmp, current, top = array.length;
+//
+//         if (top) {
+//             while (--top) {
+//                 current = Math.floor(Math.random() * (top + 1));
+//                 tmp = array[current];
+//                 array[current] = array[top];
+//                 array[top] = tmp;
+//             }
+//         }
+//
+//         return array;
+//     }
+// });
