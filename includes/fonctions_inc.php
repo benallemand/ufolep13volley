924d4e6cf12d18fd290b74e076ae4a4f23f7b2df
benallemand <benallemand@gmail.com>
2016-10-31 15:52:11 +0100

    fix issue with ranking played matches count

--- includes/fonctions_inc.php
+++ includes/fonctions_inc.php
@@ -786,7 +786,16 @@ FROM (
            THEN 1
                                ELSE 0 END)
          - c.penalite                      AS points,
-         COUNT(m.id_match)                 AS joues,
+         SUM(CASE WHEN e.id_equipe = m.id_equipe_dom AND m.score_equipe_dom = 3
+           THEN 1
+             ELSE 0 END) + SUM(CASE WHEN e.id_equipe = m.id_equipe_ext AND m.score_equipe_ext = 3
+           THEN 1
+                               ELSE 0 END) +
+         SUM(CASE WHEN e.id_equipe = m.id_equipe_dom AND m.score_equipe_ext = 3
+           THEN 1
+             ELSE 0 END) + SUM(CASE WHEN e.id_equipe = m.id_equipe_ext AND m.score_equipe_dom = 3
+           THEN 1
+                               ELSE 0 END)                 AS joues,
          SUM(CASE WHEN e.id_equipe = m.id_equipe_dom AND m.score_equipe_dom = 3
            THEN 1
              ELSE 0 END) + SUM(CASE WHEN e.id_equipe = m.id_equipe_ext AND m.score_equipe_ext = 3
