afbe1a0def4064f91df4a53c7c640b86d3da92ec
Benjamin ALLEMAND <benallemand@gmail.com>
2016-11-15 21:31:03 +0100

    only show player's belonging teams when team appears in rankings

--- includes/fonctions_inc.php
+++ includes/fonctions_inc.php
@@ -1560,6 +1560,7 @@ LEFT JOIN joueur_equipe je ON je.id_joueur = j.id
 LEFT JOIN equipes e ON e.id_equipe=je.id_equipe
 LEFT JOIN clubs c ON c.id = j.id_club
 LEFT JOIN photos p ON p.id = j.id_photo
+WHERE e.id_equipe IN (SELECT id_equipe FROM classements)
 GROUP BY full_name";
     $req = mysqli_query($db, $sql) or die('Erreur SQL !<br>' . $sql . '<br>' . mysqli_error($db));
     $results = array();
