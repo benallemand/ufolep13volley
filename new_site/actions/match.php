38c6a08cf7e3932919bf8e7dd53d42829876f18d
Benjamin ALLEMAND <benallemand@gmail.com>
2016-12-11 22:01:49 +0100

    fix issue with visibility of action in match actions column

--- new_site/actions/match.php
+++ new_site/actions/match.php
@@ -3,12 +3,14 @@ session_start();
 require_once '../../includes/fonctions_inc.php';
 if (isset($_SESSION['login']) && $_SESSION['profile_name'] == 'ADMINISTRATEUR') {
     ?>
-    <form ng-hide=""x.certif=='1' || x.sheet_received=='1' || (x.set_1_dom == '0' && x.set_1_ext == '0')" style="display: inline-block" ng-submit="declareSheetReceived(x.code_match)">
+    <form ng-hide="x.certif=='1' || x.sheet_received=='1' || (x.set_1_dom == '0' && x.set_1_ext == '0')"
+          style="display: inline-block" ng-submit="declareSheetReceived(x.code_match)">
         <button title="Accuser réception des feuilles de match" type="submit" class="btn btn-sm btn-success">
             <span class="glyphicon glyphicon-ok"></span>
         </button>
     </form>
-    <form ng-hide="x.certif=='1' || (x.set_1_dom == '0' && x.set_1_ext == '0')" style="display: inline-block" ng-submit="validateMatch(x.code_match)">
+    <form ng-hide="x.certif=='1' || (x.set_1_dom == '0' && x.set_1_ext == '0')" style="display: inline-block"
+          ng-submit="validateMatch(x.code_match)">
         <button title="Certifier le match" type="submit" class="btn btn-sm btn-success">
             <span class="glyphicon glyphicon-ok"></span>
         </button>
