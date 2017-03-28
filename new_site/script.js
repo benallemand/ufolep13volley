6edd7a0e08033f696a119127db64836d572915c0
Benjamin Allemand <ballemand@insidefr.com>
2017-03-28 16:32:27 +0200

    base64 encoding cross browser

--- new_site/script.js
+++ new_site/script.js
@@ -1,3 +1,94 @@
+var Base64 = {
+    _keyStr: "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",
+    encode: function (e) {
+        if (!e) {
+            return e;
+        }
+        var t = "";
+        var n, r, i, s, o, u, a;
+        var f = 0;
+        e = Base64._utf8_encode(e);
+        while (f < e.length) {
+            n = e.charCodeAt(f++);
+            r = e.charCodeAt(f++);
+            i = e.charCodeAt(f++);
+            s = n >> 2;
+            o = (n & 3) << 4 | r >> 4;
+            u = (r & 15) << 2 | i >> 6;
+            a = i & 63;
+            if (isNaN(r)) {
+                u = a = 64
+            } else if (isNaN(i)) {
+                a = 64
+            }
+            t = t + this._keyStr.charAt(s) + this._keyStr.charAt(o) + this._keyStr.charAt(u) + this._keyStr.charAt(a)
+        }
+        return t
+    }, decode: function (e) {
+        var t = "";
+        var n, r, i;
+        var s, o, u, a;
+        var f = 0;
+        e = e.replace(/[^A-Za-z0-9+/=]/g, "");
+        while (f < e.length) {
+            s = this._keyStr.indexOf(e.charAt(f++));
+            o = this._keyStr.indexOf(e.charAt(f++));
+            u = this._keyStr.indexOf(e.charAt(f++));
+            a = this._keyStr.indexOf(e.charAt(f++));
+            n = s << 2 | o >> 4;
+            r = (o & 15) << 4 | u >> 2;
+            i = (u & 3) << 6 | a;
+            t = t + String.fromCharCode(n);
+            if (u != 64) {
+                t = t + String.fromCharCode(r)
+            }
+            if (a != 64) {
+                t = t + String.fromCharCode(i)
+            }
+        }
+        t = Base64._utf8_decode(t);
+        return t
+    }, _utf8_encode: function (e) {
+        e = e.replace(/rn/g, "n");
+        var t = "";
+        for (var n = 0; n < e.length; n++) {
+            var r = e.charCodeAt(n);
+            if (r < 128) {
+                t += String.fromCharCode(r)
+            } else if (r > 127 && r < 2048) {
+                t += String.fromCharCode(r >> 6 | 192);
+                t += String.fromCharCode(r & 63 | 128)
+            } else {
+                t += String.fromCharCode(r >> 12 | 224);
+                t += String.fromCharCode(r >> 6 & 63 | 128);
+                t += String.fromCharCode(r & 63 | 128)
+            }
+        }
+        return t
+    }, _utf8_decode: function (e) {
+        var t = "";
+        var n = 0;
+        var r = c1 = c2 = 0;
+        while (n < e.length) {
+            r = e.charCodeAt(n);
+            if (r < 128) {
+                t += String.fromCharCode(r);
+                n++
+            } else if (r > 191 && r < 224) {
+                c2 = e.charCodeAt(n + 1);
+                t += String.fromCharCode((r & 31) << 6 | c2 & 63);
+                n += 2
+            } else {
+                c2 = e.charCodeAt(n + 1);
+                c3 = e.charCodeAt(n + 2);
+                t += String.fromCharCode((r & 15) << 12 | (c2 & 63) << 6 | c3 & 63);
+                n += 3
+            }
+        }
+        return t
+    }
+}
+
 // create the module and name it scotchApp
 var scotchApp = angular.module('scotchApp', ['ngRoute', 'ngAnimate', 'ui.bootstrap', 'angular.filter']);
 
@@ -417,10 +508,10 @@ scotchApp.controller('myTeamController', ['$scope', '$http', 'multipartForm', fu
     $http.get("../ajax/getMonEquipe.php")
         .then(function (response) {
             $scope.team = response.data[0];
-            $scope.team["responsable_base64"] = window.btoa($scope.team["responsable"]);
-            $scope.team["telephone_1_base64"] = window.btoa($scope.team["telephone_1"]);
-            $scope.team["telephone_2_base64"] = window.btoa($scope.team["telephone_2"]);
-            $scope.team["email_base64"] = window.btoa($scope.team["email"]);
+            $scope.team["responsable_base64"] = Base64.encode($scope.team["responsable"]);
+            $scope.team["telephone_1_base64"] = Base64.encode($scope.team["telephone_1"]);
+            $scope.team["telephone_2_base64"] = Base64.encode($scope.team["telephone_2"]);
+            $scope.team["email_base64"] = Base64.encode($scope.team["email"]);
             $scope.modify_my_team.web_site = $scope.team.web_site;
             $scope.modify_my_team.id_club = $scope.team.id_club;
         });
@@ -564,10 +655,10 @@ scotchApp.controller('myPageController', function ($scope, $http) {
     $http.get("../ajax/getMonEquipe.php")
         .then(function (response) {
             $scope.team = response.data[0];
-            $scope.team["responsable_base64"] = window.btoa($scope.team["responsable"]);
-            $scope.team["telephone_1_base64"] = window.btoa($scope.team["telephone_1"]);
-            $scope.team["telephone_2_base64"] = window.btoa($scope.team["telephone_2"]);
-            $scope.team["email_base64"] = window.btoa($scope.team["email"]);
+            $scope.team["responsable_base64"] = Base64.encode($scope.team["responsable"]);
+            $scope.team["telephone_1_base64"] = Base64.encode($scope.team["telephone_1"]);
+            $scope.team["telephone_2_base64"] = Base64.encode($scope.team["telephone_2"]);
+            $scope.team["email_base64"] = Base64.encode($scope.team["email"]);
         });
 
     $scope.askForReport = function (code_match) {
@@ -707,11 +798,11 @@ scotchApp.controller('commissionController', function ($scope, $http) {
             var commission_team = $scope.commission;
             var commission_team_count = commission_team.length;
             for (var currentIndex = 0; currentIndex < commission_team_count; currentIndex++) {
-                $scope.commission[currentIndex]["prenom_base64"] = window.btoa($scope.commission[currentIndex]["prenom"]);
-                $scope.commission[currentIndex]["nom_base64"] = window.btoa($scope.commission[currentIndex]["nom"]);
-                $scope.commission[currentIndex]["telephone1_base64"] = window.btoa($scope.commission[currentIndex]["telephone1"]);
-                $scope.commission[currentIndex]["telephone2_base64"] = window.btoa($scope.commission[currentIndex]["telephone2"]);
-                $scope.commission[currentIndex]["email_base64"] = window.btoa($scope.commission[currentIndex]["email"]);
+                $scope.commission[currentIndex]["prenom_base64"] = Base64.encode($scope.commission[currentIndex]["prenom"]);
+                $scope.commission[currentIndex]["nom_base64"] = Base64.encode($scope.commission[currentIndex]["nom"]);
+                $scope.commission[currentIndex]["telephone1_base64"] = Base64.encode($scope.commission[currentIndex]["telephone1"]);
+                $scope.commission[currentIndex]["telephone2_base64"] = Base64.encode($scope.commission[currentIndex]["telephone2"]);
+                $scope.commission[currentIndex]["email_base64"] = Base64.encode($scope.commission[currentIndex]["email"]);
             }
         });
 });
@@ -724,10 +815,10 @@ scotchApp.controller('phonebookController', ['$scope', '$routeParams', '$http', 
     })
         .then(function (response) {
             $scope.team = response.data;
-            $scope.team["responsable_base64"] = window.btoa($scope.team["responsable"]);
-            $scope.team["telephone_1_base64"] = window.btoa($scope.team["telephone_1"]);
-            $scope.team["telephone_2_base64"] = window.btoa($scope.team["telephone_2"]);
-            $scope.team["email_base64"] = window.btoa($scope.team["email"]);
+            $scope.team["responsable_base64"] = Base64.encode($scope.team["responsable"]);
+            $scope.team["telephone_1_base64"] = Base64.encode($scope.team["telephone_1"]);
+            $scope.team["telephone_2_base64"] = Base64.encode($scope.team["telephone_2"]);
+            $scope.team["email_base64"] = Base64.encode($scope.team["email"]);
         });
 }]);
 
