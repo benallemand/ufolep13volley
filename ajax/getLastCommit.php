2710dbf7169fea40710258b9400bb3919301a007
Benjamin Allemand <ballemand@insidefr.com>
2017-10-03 14:17:33 +0200

    refactor home page to display last commit

--- ajax/getLastCommit.php
+++ ajax/getLastCommit.php
@@ -1,73 +1,20 @@
 <?php
 
-function get_json($url)
-{
-    $base = "https://api.github.com/";
-    $curl = curl_init();
-    curl_setopt($curl, CURLOPT_URL, $base . $url);
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
-    curl_setopt($curl, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows; U; Windows NT 5.0; en-US; rv:1.4) Gecko/20030624 Netscape/7.1 (ax)");
-
-    //curl_setopt($curl, CONNECTTIMEOUT, 1);
-    $content = curl_exec($curl);
-    curl_close($curl);
-    return $content;
-}
-
-function get_latest_repo($user)
-{
-    // Get the json from github for the repos
-    $json = json_decode(get_json("users/$user/repos"), true);
-    if (isset($json['documentation_url']) && $json['documentation_url'] == "https://developer.github.com/v3/#rate-limiting") {
-        echo "Derniere modification indisponible pour le moment";
-        exit(0);
-    }
-
-    // Sort the array returend by pushed_at time
-    function compare_pushed_at($b, $a)
-    {
-        return strnatcmp($a['pushed_at'], $b['pushed_at']);
-    }
-
-    usort($json, 'compare_pushed_at');
-
-    //Now just get the latest repo
-    $json = $json[0];
-
-    return $json;
-}
-
-function get_commits($repo, $user)
-{
-    // Get the name of the repo that we'll use in the request url
-    $repoName = $repo["name"];
-    return json_decode(get_json("repos/$user/$repoName/commits"), true);
-}
-
+require_once '../libs/Unirest.php';
 $serverName = filter_input(INPUT_SERVER, 'SERVER_NAME');
+$url = "https://api.github.com/repos/benallemand/ufolep13volley/commits";
+Unirest\Request::verifyPeer(false);
 switch ($serverName) {
     case 'localhost':
-        echo '';
+        Unirest\Request::proxy('aixproxyprod.insidefr.com', 3128, CURLPROXY_HTTP);
         break;
     default:
-        $login = "benallemand";
-        $latestRepo = get_latest_repo($login);
-        $commits = get_commits($latestRepo, $login);
-        $latestCommit = $commits[0];
-        $repoURL = $latestRepo["html_url"];
-        $repoName = $latestRepo["name"];
-        $repoDescription = $latestRepo["description"];
-        $gravatar = $latestRepo["owner"]["avatar_url"];
-        $author = $latestCommit["commit"]["author"]["name"];
-        $dateLast = $latestCommit["commit"]["author"]["date"];
-        $userURL = "https://github.com/$login";
-        $commitMessage = $latestCommit["commit"]["message"];
-        $commitSHA = $latestCommit["sha"];
-        $commitURL = "https://github.com/$login/$repoName/commit/$commitSHA";
-        $time = strtotime($dateLast);
-        $formattedLastDate = date('d/m/Y H:i:s', $time);
-        echo "Derniere modification: (<a href='$commitURL'>$formattedLastDate</a>)";
         break;
 }
-
+$response = Unirest\Request::get($url);
+$test = json_decode($response->raw_body);
+$commitURL = $test[0]->commit->url;
+date_default_timezone_set('Europe/Paris');
+$time = strtotime($test[0]->commit->committer->date);
+$formattedLastDate = date('d/m/Y H:i:s', $time);
+echo "Derniere modification: (<a href='$commitURL'>$formattedLastDate</a>)";
