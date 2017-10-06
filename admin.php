6978a7aab409089bd6565c0a98fa01609f4530ed
Benjamin Allemand <ballemand@insidefr.com>
2017-10-06 15:43:40 +0200

    update lib extjs usage for admin portal

--- admin.php
+++ admin.php
@@ -8,19 +8,41 @@ if (!isAdmin()) {
 <HTML>
 <HEAD>
     <TITLE>Administration UFOLEP 13 VOLLEY</TITLE>
-    <META http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-    <link rel="shortcut icon" href="favicon.ico"/>
-    <link href="includes/main.css" rel="stylesheet" type="text/css" media="screen"/>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css"/>
-    <link href="images/fonts/icomoon/style.css" rel="stylesheet" type="text/css" media="screen"/>
+    <META
+            http-equiv="Content-Type"
+            content="text/html; charset=utf-8"/>
     <link
-        href="https://cdn.sencha.com/ext/gpl/5.1.0/build/packages/ext-theme-neptune/build/resources/ext-theme-neptune-all-debug.css"
-        rel="stylesheet"/>
-    <script src="https://cdn.sencha.com/ext/gpl/5.1.0/build/ext-all.js" type="text/javascript"></script>
-    <script src="https://cdn.sencha.com/ext/gpl/5.1.0/build/packages/ext-locale/build/ext-locale-fr.js"
+            rel="shortcut icon"
+            href="favicon.ico"/>
+    <link
+            href="includes/main.css"
+            rel="stylesheet"
+            type="text/css"
+            media="screen"/>
+    <link
+            rel="stylesheet"
+            href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css"/>
+    <link
+            href="images/fonts/icomoon/style.css"
+            rel="stylesheet"
+            type="text/css"
+            media="screen"/>
+    <link
+            href="//cdnjs.cloudflare.com/ajax/libs/extjs/6.2.0/classic/theme-neptune/resources/theme-neptune-all.css"
+            rel="stylesheet"/>
+    <script
+            src="//cdnjs.cloudflare.com/ajax/libs/extjs/6.2.0/ext-all.js"
             type="text/javascript"></script>
-    <script type="text/javascript" src="js/libs/Commons.js"></script>
-    <script type="text/javascript" src="js/administration.js"></script>
+    <script
+            src="//cdnjs.cloudflare.com/ajax/libs/extjs/6.2.0/classic/locale/locale-fr.js"
+            type="text/javascript"></script>
+    <script
+            src="//cdnjs.cloudflare.com/ajax/libs/extjs/6.2.0/classic/theme-neptune/theme-neptune.js"
+            type="text/javascript"></script>
+    <script
+            type="text/javascript" src="js/libs/Commons.js"></script>
+    <script
+            type="text/javascript" src="js/administration.js"></script>
 </HEAD>
 <BODY>
 </BODY>
