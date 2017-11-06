eddbaedfba03718a14bdfb6647b01b8af7001ee4
Benjamin Allemand <ballemand@insidefr.com>
2017-11-06 17:05:23 +0100

    ajout de la gestion des créneaux

--- js/store/Timeslots.js
+++ js/store/Timeslots.js
@@ -4,7 +4,7 @@ Ext.define('Ufolep13Volley.store.Timeslots', Sencha.storeCompatibility({
         model: 'Ufolep13Volley.model.Timeslot',
         proxy: {
             type: 'ajax',
-            url: 'ajax/getTimeslots.php'
+            url: 'ajax/getTimeSlots.php'
         },
         autoLoad: true
     }
