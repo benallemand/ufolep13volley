bcd11015b38306b19a729bf90c87640f87359364
benallemand <benallemand@gmail.com>
2018-01-16 10:46:21 +0100

    days generation

---
+++ ajax/generateDays.php
@@ -0,0 +1,28 @@
+<?php
+
+try {
+    $requestMethod = filter_input(INPUT_SERVER, 'REQUEST_METHOD');
+    switch ($requestMethod) {
+        case 'POST':
+            $code_competition = filter_input(INPUT_POST, 'code_competition');
+            break;
+        case 'GET':
+            $code_competition = filter_input(INPUT_GET, 'code_competition');
+            break;
+        default:
+            throw new Exception("Request not allowed");
+    }
+    require_once '../classes/MatchManager.php';
+    $manager = new MatchManager();
+    $manager->generateDays($code_competition);
+} catch (Exception $ex) {
+    echo json_encode(array(
+        'success' => false,
+        'message' => 'Erreur durant la modification: ' . $ex->getMessage()
+    ));
+    return;
+}
+echo json_encode(array(
+    'success' => true,
+    'message' => 'Modification OK'
+));
--- classes/DayManager.php
+++ classes/DayManager.php
@@ -10,11 +10,62 @@ require_once 'Generic.php';
 
 class DayManager extends Generic
 {
-    public function insertDay($code_competition, $numero)
+
+    /**
+     * @param null $query
+     * @return string
+     */
+    private function getSql($query = null)
+    {
+        $sql = "SELECT 
+        j.id,
+        j.code_competition,
+        j.numero,
+        j.nommage,
+        j.libelle,
+        DATE_FORMAT(j.start_date, '%d/%m/%Y') AS start_date
+        FROM journees j
+        WHERE 1=1";
+        if ($query !== NULL) {
+            $sql .= " AND $query";
+        }
+        return $sql;
+    }
+
+    /**
+     * @param null $query
+     * @return array
+     * @throws Exception
+     */
+    public function getDays($query = null)
+    {
+        $db = Database::openDbConnection();
+        $sql = $this->getSql($query);
+        $req = mysqli_query($db, $sql) or die('Erreur SQL !<br>' . $sql . '<br>' . mysqli_error($db));
+        $results = array();
+        while ($data = mysqli_fetch_assoc($req)) {
+            $results[] = $data;
+        }
+        return $results;
+    }
+
+    /**
+     * @param $code_competition
+     * @param $numero
+     * @param $competition_start_date
+     * @return int|string
+     * @throws Exception
+     */
+    public function insertDay($code_competition, $numero, $competition_start_date)
     {
         $db = Database::openDbConnection();
         $numero_padded = str_pad($numero, 2, '0', STR_PAD_LEFT);
-        $sql = "INSERT INTO journees SET code_competition = '$code_competition', numero = $numero, nommage = 'Journee $numero_padded'";
+        $week_offset = $numero - 1;
+        $sql = "INSERT INTO journees SET 
+          code_competition = '$code_competition', 
+          numero = $numero, 
+          nommage = 'Journee $numero_padded',
+          start_date = ADDDATE(STR_TO_DATE('$competition_start_date', '%d/%m/%Y'), INTERVAL $week_offset WEEK)";
         $req = mysqli_query($db, $sql);
         if ($req === FALSE) {
             $message = mysqli_error($db);
@@ -22,6 +73,19 @@ class DayManager extends Generic
             throw new Exception($message);
         }
         return mysqli_insert_id($db);
+    }
+
+    public function deleteDays($query)
+    {
+        $db = Database::openDbConnection();
+        $sql = "DELETE FROM journees WHERE 1=1";
+        if ($query !== NULL) {
+            $sql .= " AND $query";
+        }
+        $req = mysqli_query($db, $sql);
+        if ($req === FALSE) {
+            throw new Exception("Erreur durant l'effacement: " . mysqli_error($db));
+        }
     }
 
 }
