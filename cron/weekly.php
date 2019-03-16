fd34b115a1f425134ad2b16dfe06c46b2b9e5d16
Benjamin ALLEMAND <benallemand@gmail.com>
2019-03-16 19:44:35 +0100

    ajout de la tâche de clean des fichiers en doublon (chaque semaine)

--- cron/weekly.php
+++ cron/weekly.php
@@ -7,6 +7,7 @@ try {
     $cron_tasks->sendMailPlayersWithoutLicenceNumber();
     $cron_tasks->sendMailTeamLeadersWithoutEmail();
     $cron_tasks->sendMailAlertReport();
+    $cron_tasks->cleanupFiles();
 } catch (Exception $exception) {
     echo json_encode(array(
         'success' => false,
