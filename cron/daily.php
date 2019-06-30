5195078c0d877faf7c65724a2ed73d6ed5037b4a
Benjamin ALLEMAND <benallemand@gmail.com>
2019-06-30 18:25:56 +0200

    ufolep13volley-135 Annuler les envois des rapports d’activité quotidiens

--- cron/daily.php
+++ cron/daily.php
@@ -2,7 +2,6 @@
 try {
     require_once __DIR__ . '/../classes/CronTasks.php';
     $cron_tasks = new CronTasks();
-    $cron_tasks->sendMailActivity();
 } catch (Exception $exception) {
     echo json_encode(array(
         'success' => false,
