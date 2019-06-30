cefd65470413ee1889dd2cbb9b7041881c9b175c
Benjamin ALLEMAND <benallemand@gmail.com>
2019-06-30 18:17:08 +0200

    ufolep13volley-139 Ajouter un pied de page pour la beauté du site

---
+++ new_site/pages/footer.php
@@ -0,0 +1,5 @@
+<footer class="footer">
+    <div class="container">
+        <i class="glyphicon glyphicon-copyright-mark" aria-hidden="true"></i> <?php echo date('Y');?> - UFOLEP 13 SECTION VOLLEY</span>
+    </div>
+</footer>
