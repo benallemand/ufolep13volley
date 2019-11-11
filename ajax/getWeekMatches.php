a3438a281b0e3c8aefa4baafa22e210356b7ce5a
Benjamin ALLEMAND <benallemand@gmail.com>
2019-11-11 20:17:59 +0100

    correction matches de la semaine

--- ajax/getWeekMatches.php
+++ ajax/getWeekMatches.php
@@ -2,7 +2,7 @@
 
 require_once __DIR__ . "/../includes/fonctions_inc.php";
 
-$date_string = date('%d/%m/%Y');
+$date_string = date('d/m/Y');
 //$date_string = "06/06/2019";
 if (filter_input(INPUT_GET, 'callback') !== null) {
     echo filter_input(INPUT_GET, 'callback') . "(" . getWeekMatches($date_string) . ")";
