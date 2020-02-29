600f794609687357463c0dfeaef27a779ed9bf44
Benjamin ALLEMAND <benallemand@gmail.com>
2020-02-29 17:21:37 +0100

    indicateur sur les problèmes de date

--- ajax/indicators.php
+++ ajax/indicators.php
@@ -418,6 +418,75 @@ $indicatorMatchesWithInvalidPlayers = new Indicator(
                 AND m.match_status = 'CONFIRMED'
           order by nom");
 
+$indicatorMatchesWithInvalidDate = new Indicator(
+    "Problèmes dans les dates des matchs",
+    "select m.code_match,
+       m.date_reception,
+       edom.nom_equipe  AS domicile,
+       eext.nom_equipe  AS exterieur,
+       m.match_status   AS statut,
+       'date interdite' AS raison
+from matches m
+         JOIN equipes edom on m.id_equipe_dom = edom.id_equipe
+         JOIN equipes eext on m.id_equipe_ext = eext.id_equipe
+WHERE (m.date_reception IN (SELECT closed_date FROM blacklist_date))
+  AND m.match_status != 'ARCHIVED'
+UNION ALL
+select m.code_match,
+       m.date_reception,
+       edom.nom_equipe   AS domicile,
+       eext.nom_equipe   AS exterieur,
+       m.match_status    AS statut,
+       'gymnase indispo' AS raison
+from matches m
+         JOIN equipes edom on m.id_equipe_dom = edom.id_equipe
+         JOIN equipes eext on m.id_equipe_ext = eext.id_equipe
+         JOIN creneau c on edom.id_equipe = c.id_equipe
+         JOIN blacklist_gymnase bg on c.id_gymnase = bg.id_gymnase
+WHERE bg.closed_date = m.date_reception
+  AND m.match_status != 'ARCHIVED'
+UNION ALL
+select m.code_match,
+       m.date_reception,
+       edom.nom_equipe           AS domicile,
+       eext.nom_equipe           AS exterieur,
+       m.match_status            AS statut,
+       'equipe domicile indispo' AS raison
+from matches m
+         JOIN equipes edom on m.id_equipe_dom = edom.id_equipe
+         JOIN equipes eext on m.id_equipe_ext = eext.id_equipe
+         JOIN blacklist_team bt on edom.id_equipe = bt.id_team
+WHERE bt.closed_date = m.date_reception
+  AND m.match_status != 'ARCHIVED'
+UNION ALL
+select m.code_match,
+       m.date_reception,
+       edom.nom_equipe            AS domicile,
+       eext.nom_equipe            AS exterieur,
+       m.match_status             AS statut,
+       'equipe extérieur indispo' AS raison
+from matches m
+         JOIN equipes edom on m.id_equipe_dom = edom.id_equipe
+         JOIN equipes eext on m.id_equipe_ext = eext.id_equipe
+         JOIN blacklist_team bt on eext.id_equipe = bt.id_team
+WHERE bt.closed_date = m.date_reception
+  AND m.match_status != 'ARCHIVED'
+UNION ALL
+select m_t1.code_match || ' et ' || m_t2.code_match    AS code_match,
+       m_t1.date_reception,
+       edom.nom_equipe                                 AS domicile,
+       eext.nom_equipe                                 AS exterieur,
+       m_t1.match_status                               AS statut,
+       'equipes qui ne peuvent pas jouer le même soir' AS raison
+FROM blacklist_teams bt
+         JOIN matches m_t1 ON m_t1.id_equipe_dom = bt.id_team_1 OR m_t1.id_equipe_ext = bt.id_team_1
+         JOIN matches m_t2 ON m_t2.id_equipe_dom = bt.id_team_2 OR m_t2.id_equipe_ext = bt.id_team_2
+         JOIN equipes edom on bt.id_team_1 = edom.id_equipe
+         JOIN equipes eext on bt.id_team_2 = eext.id_equipe
+WHERE m_t1.date_reception = m_t2.date_reception
+  AND m_t1.match_status != 'ARCHIVED'
+order by code_match");
+
 $results = array();
 $results[] = $indicatorEquipesEngageesChampionnat->getResult();
 $results[] = $indicatorPlayersWithTeamButNoClub->getResult();
@@ -443,6 +512,7 @@ $results[] = $indicatorEquityBetweenHomeAndAway->getResult();
 $results[] = $indicatorMatchGenerationCriticity->getResult();
 $results[] = $indicatorErrorInEmails->getResult();
 $results[] = $indicatorMatchesWithInvalidPlayers->getResult();
+$results[] = $indicatorMatchesWithInvalidDate->getResult();
 
 $indicatorName = filter_input(INPUT_GET, 'indicator');
 if (!$indicatorName) {
