bb92a48de5b4c07b0a94a86d9215db2b5af3ce74
Benjamin ALLEMAND <benallemand@gmail.com>
2022-10-19 11:45:11 +0200

    inscription

--- js/view/grid/register.js
+++ js/view/grid/register.js
@@ -2,19 +2,33 @@ Ext.define('Ufolep13Volley.view.grid.register', {
     extend: 'Ufolep13Volley.view.grid.ufolep',
     alias: 'widget.grid_register',
     title: 'Equipes inscrites',
-    store: 'register',
+    store:
+        {
+            type: 'register',
+            sorters: [
+                {
+                    property: 'competition',
+                    direction: 'ASC'
+                },
+                {
+                    property: 'club',
+                    direction: 'ASC'
+                }
+            ]
+        },
     selType: 'rowmodel',
     autoScroll: true,
     columns: [
         {header: "Nom d'équipe", dataIndex: 'new_team_name', width: 200,},
+        {header: 'Compétition', dataIndex: 'competition', width: 200,},
         {header: 'Club', dataIndex: 'club', width: 200,},
         {header: 'Ancien nom', dataIndex: 'old_team', width: 200,},
         {header: 'Responsable', dataIndex: 'leader', width: 200,},
-        {header: 'Gymnase 1', dataIndex: 'court_1', hidden: true, width: 200,},
-        {header: 'Jour 1', dataIndex: 'day_court_1', hidden: true, width: 200,},
-        {header: 'Heure 1', dataIndex: 'hour_court_1', hidden: true, width: 200,},
-        {header: 'Gymnase 2', dataIndex: 'court_2', hidden: true, width: 200,},
-        {header: 'Jour 2', dataIndex: 'day_court_2', hidden: true, width: 200,},
-        {header: 'Heure 2', dataIndex: 'hour_court_2', hidden: true, width: 200,},
+        {header: 'Gymnase 1', dataIndex: 'court_1', hidden: false, width: 200,},
+        {header: 'Jour 1', dataIndex: 'day_court_1', hidden: false, width: 200,},
+        {header: 'Heure 1', dataIndex: 'hour_court_1', hidden: false, width: 200,},
+        {header: 'Gymnase 2', dataIndex: 'court_2', hidden: false, width: 200,},
+        {header: 'Jour 2', dataIndex: 'day_court_2', hidden: false, width: 200,},
+        {header: 'Heure 2', dataIndex: 'hour_court_2', hidden: false, width: 200,},
     ],
 });
