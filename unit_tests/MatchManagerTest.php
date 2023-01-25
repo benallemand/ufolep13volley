43a19a38d321b1e9224577ec969c60980c95fdda
Benjamin ALLEMAND <benallemand@gmail.com>
2023-01-25 17:55:15 +0100

    ajout de tests unitaires

--- unit_tests/MatchManagerTest.php
+++ unit_tests/MatchManagerTest.php
@@ -425,13 +425,22 @@ class MatchManagerTest extends TestCase
     /**
      * @throws Exception
      */
-    public function test_generate_all()
+    public function test_generate_all_kh()
     {
         //230105:PASS
         $competition_mgr = new Competition();
         $competition_kh = $competition_mgr->getCompetition('kh');
         $this->match_manager->generateAll($competition_kh['id']);
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function test_generate_all_isoardi()
+    {
+        //230105:PASS
         // test for isoardi, where registration is automatic
+        $competition_mgr = new Competition();
         $competition_isoardi = $competition_mgr->getCompetition('c');
         $this->match_manager->generateAll($competition_isoardi['id']);
     }
--- unit_tests/RegisterTest.php
+++ unit_tests/RegisterTest.php
@@ -28,8 +28,8 @@ class RegisterTest extends TestCase
     }
 
     /**
- * @throws Exception
- */
+     * @throws Exception
+     */
     public function test_set_up_season_isoardi()
     {
         //230105:PASS
@@ -43,18 +43,6 @@ class RegisterTest extends TestCase
      * @throws Exception
      */
     public function test_get_pending_registrations()
-    {
-        //230123:PASS
-        $competition_mgr = new Competition();
-        $competition_kh = $competition_mgr->getCompetition('kh');
-        $pending_registrations = $this->register->get_pending_registrations($competition_kh['id']);
-        $this->assertNotEmpty($pending_registrations);
-    }
-
-    /**
-     * @throws Exception
-     */
-    public function test_group_draw()
     {
         //230123:PASS
         $competition_mgr = new Competition();
