f49f28feae907bedde7e6964aae173a7cc643ce4
Benjamin ALLEMAND <benallemand@gmail.com>
2023-01-25 11:06:34 +0100

    ajout de tests unitaires

--- unit_tests/RegisterTest.php
+++ unit_tests/RegisterTest.php
@@ -19,15 +19,48 @@ class RegisterTest extends TestCase
     /**
      * @throws Exception
      */
-    public function test_set_up_season()
+    public function test_set_up_season_kh()
     {
         //230105:PASS
         $competition_mgr = new Competition();
         $competition_kh = $competition_mgr->getCompetition('kh');
         $this->register->set_up_season($competition_kh['id']);
+    }
+
+    /**
+ * @throws Exception
+ */
+    public function test_set_up_season_isoardi()
+    {
+        //230105:PASS
+        $competition_mgr = new Competition();
         // test for isoardi, where registration is automatic
         $competition_isoardi = $competition_mgr->getCompetition('c');
         $this->register->set_up_season($competition_isoardi['id']);
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function test_get_pending_registrations()
+    {
+        //230123:PASS
+        $competition_mgr = new Competition();
+        $competition_kh = $competition_mgr->getCompetition('kh');
+        $pending_registrations = $this->register->get_pending_registrations($competition_kh['id']);
+        $this->assertNotEmpty($pending_registrations);
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function test_group_draw()
+    {
+        //230123:PASS
+        $competition_mgr = new Competition();
+        $competition_kh = $competition_mgr->getCompetition('kh');
+        $pending_registrations = $this->register->get_pending_registrations($competition_kh['id']);
+        $this->assertNotEmpty($pending_registrations);
     }
 
 }
