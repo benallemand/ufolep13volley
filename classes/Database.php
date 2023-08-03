6de2f14f2a168b4344216224078c6c0ab57f2372
Benjamin ALLEMAND <benallemand@gmail.com>
2023-08-03 23:02:19 +0200

    mise à jour du .env pour gérer la connexion à la base

--- classes/Database.php
+++ classes/Database.php
@@ -1,5 +1,6 @@
 <?php
 require __DIR__ . '/../vendor/autoload.php';
+
 use Dotenv\Dotenv;
 
 class Database
@@ -14,33 +15,23 @@ class Database
     {
         $dotenv = Dotenv::createImmutable(__DIR__ . '/../');
         $dotenv->load();
-        $dotenv->required(['DB_NAME', 'DB_USER', 'DB_PASSWORD']);
+        $dotenv->required([
+            'DB_NAME',
+            'DB_USER',
+            'DB_PASSWORD',
+            'DB_SERVER',
+            'DB_SOCKET',
+            'DB_PORT',
+        ]);
         if (!is_null(self::$_db)) {
             return self::$_db;
         }
-//        $serverName = filter_input(INPUT_SERVER, 'SERVER_NAME');
-        $serverName = $_SERVER['SERVER_NAME'];
         $user = $_ENV['DB_USER'];
         $base = $_ENV['DB_NAME'];
         $password = $_ENV['DB_PASSWORD'];
-        $socket = null;
-        $port = null;
-        switch ($serverName) {
-            case 'localhost':
-            case null:
-                $server = "localhost";
-                $socket = '/var/run/mysqld/mysqld.sock';
-                $port = 3306;
-                break;
-            case 'www.ufolep13volley.org':
-            case 'ufolep13volley.org':
-            case '':
-            case 'ufolepvocb.cluster020.hosting.ovh.net':
-                $server = "ufolepvocbufolep.mysql.db";
-                break;
-            default:
-                throw new Exception("Le nom du serveur nest pas autorisé pour la connexion SQL: $serverName");
-        }
+        $server = $_ENV['DB_SERVER'];
+        $socket = $_ENV['DB_SOCKET'];
+        $port = $_ENV['DB_PORT'];
         self::$_db = mysqli_connect($server, $user, $password, $base, $port, $socket);
         if (self::$_db === false) {
             throw new Exception("Impossible de se connecter à la base de données !");
