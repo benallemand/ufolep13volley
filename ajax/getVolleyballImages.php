1610b15e6a9089db0531e9cc62178326e7c23a2e
Benjamin ALLEMAND <benallemand@gmail.com>
2023-08-06 11:44:10 +0200

    passage en configuration de flickr api key et proxy url

--- ajax/getVolleyballImages.php
+++ ajax/getVolleyballImages.php
@@ -1,29 +1,23 @@
 <?php
 require_once __DIR__ . '/../vendor/autoload.php';
-require_once __DIR__ . '/../includes/conf.php';
-global $flickr_api_key;
-global $proxy_url;
-$url = "https://api.flickr.com/services/rest?method=flickr.photos.search&sort=relevance&per_page=50&api_key=$flickr_api_key&text=volleyball&format=json&nojsoncallback=1";
-//$url = "https://api.flickr.com/services/rest/?method=flickr.people.getPhotos&api_key=$flickr_api_key&user_id=42227760@N04&format=json&nojsoncallback=1";
+require_once __DIR__ . '/../classes/Configuration.php';
+$configuration = new Configuration();
+$flickr_api_key = $configuration->flickr_api_key;
+$proxy_url = $configuration->proxy_url;
 Unirest\Request::verifyPeer(false);
-try {
-    $headers = array('Accept' => 'application/json');
-    $query = array(
-        'method' => 'flickr.photos.search',
-        'sort' => 'relevance',
-        'per_page' => '10',
-        'api_key' => $flickr_api_key,
-        'text' => 'volleyball',
-        'format' => 'json',
-        'nojsoncallback' => 1,
-        );
-
-    $response = Unirest\Request::get('https://api.flickr.com/services/rest', $headers, $query);
-//    $response = Unirest\Request::get($url);
-} catch (Unirest\Exception $e) {
-    // If localhost and proxy needed
+if (!empty($proxy_url)) {
     Unirest\Request::proxy($proxy_url, 3128, CURLPROXY_HTTP);
-    $response = Unirest\Request::get($url);
 }
+$headers = array('Accept' => 'application/json');
+$query = array(
+    'method' => 'flickr.photos.search',
+    'sort' => 'relevance',
+    'per_page' => '10',
+    'api_key' => $flickr_api_key,
+    'text' => 'volleyball',
+    'format' => 'json',
+    'nojsoncallback' => 1,
+);
+$response = Unirest\Request::get('https://api.flickr.com/services/rest', $headers, $query);
 $test = json_decode($response->raw_body);
 echo json_encode($test->photos);
