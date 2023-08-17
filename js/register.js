146596b780560a889726b3c2e97539eb7b9ff5a1
Benjamin ALLEMAND <benallemand@gmail.com>
2023-08-17 10:07:19 +0200

    inscription: nouveau bandeau supérieur

--- js/register.js
+++ js/register.js
@@ -9,17 +9,23 @@ Ext.application({
             items: [
                 {
                     region: 'north',
+                    autoScroll: true,
                     margin: 10,
                     defaults: {
                         xtype: 'button',
                         scale: 'medium',
                         margin: 5,
                     },
+                    layout: 'hbox',
                     items: [
                         {
                             text: "RETOUR A L'ACCUEIL",
                             iconCls: 'fa-solid fa-house',
                             href: '/',
+                        },
+                        {
+                            xtype: 'label',
+                            html: Ext.String.format("<div>Nous sommes le : {0}</div>", Ext.Date.format(new Date(), 'd/m/Y')),
                         },
                         {
                             xtype: 'label',
