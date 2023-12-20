87f12c03200fea297501bebdbc88cc45d8ab2df4
Benjamin ALLEMAND <benallemand@gmail.com>
2023-12-20 19:25:36 +0100

    bug fix indicateur des joueurs non valides

--- sql/match_invalid_players.sql
+++ sql/match_invalid_players.sql
@@ -8,6 +8,6 @@ from matches m
          join match_player mp on m.id_match = mp.id_match
          join players_view j on mp.id_player = j.id
          join clubs c on j.id_club = c.id
-where (j.date_homologation > m.date_reception OR j.est_actif = 0)
+where (STR_TO_DATE(j.date_homologation, '%d/%m/%Y') > m.date_reception OR j.est_actif = 0)
   AND m.match_status = 'CONFIRMED'
 order by date_reception, code_match
