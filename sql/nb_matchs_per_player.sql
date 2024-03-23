f4d4e92df4dc20911d6cf5284b5c053f69878401
Benjamin ALLEMAND <benallemand@gmail.com>
2024-03-23 14:09:00 +0100

    ajout d'un indicateur sur le nombre de matchs joués par un joueur

--- sql/nb_matchs_per_player.sql
+++ sql/nb_matchs_per_player.sql
@@ -1,16 +1,15 @@
 SELECT c.nom                                                              as club,
-       e.nom_equipe                                                       as equipe,
-       CONCAT(p.prenom, ' ', p.nom)                                       AS joueur,
-       COUNT(DISTINCT m.id_match)                                         AS nb_matchs_joues,
+       GROUP_CONCAT(DISTINCT e.nom_equipe)                                as equipes,
+       CONCAT(j.prenom, ' ', j.nom)                                       AS joueur,
+       COUNT(DISTINCT mp.id_match)                                        AS nb_matchs_joues,
        GROUP_CONCAT(DISTINCT m.code_match ORDER BY m.date_reception DESC) AS derniers_matchs
 FROM match_player mp
          JOIN matches m ON mp.id_match = m.id_match
-         JOIN players_view p ON p.id = mp.id_player
-         JOIN joueur_equipe je ON je.id_joueur = p.id
+         JOIN joueurs j ON j.id = mp.id_player
+         JOIN joueur_equipe je ON je.id_joueur = j.id AND je.id_equipe IN (m.id_equipe_dom, m.id_equipe_ext)
          JOIN equipes e ON je.id_equipe = e.id_equipe
-         JOIN clubs c ON c.id = p.id_club
-GROUP BY CONCAT(p.prenom, ' ', p.nom), c.nom, e.nom_equipe
+         JOIN clubs c ON c.id = j.id_club
+GROUP BY CONCAT(j.prenom, ' ', j.nom)
 ORDER BY nb_matchs_joues,
          club,
-         equipe,
          joueur
