c934d365c43c0d9b12cb72db33303f080634ad0e
Benjamin ALLEMAND <benallemand@gmail.com>
2024-11-05 06:19:17 +0100

    grille générique: champ recherche intégré

--- js/view/grid/ufolep.js
+++ js/view/grid/ufolep.js
@@ -3,4 +3,25 @@ Ext.define('Ufolep13Volley.view.grid.ufolep', {
     alias: 'widget.grid_ufolep',
     autoScroll: true,
     selType: 'checkboxmodel',
+    dockedItems: [
+        {
+            xtype: 'toolbar',
+            dock: 'top',
+            items: [
+                'FILTRES',
+                {
+                    xtype: 'tbseparator'
+                },
+                {
+                    xtype: 'textfield',
+                    fieldLabel: 'Recherche'
+                },
+                {
+                    xtype: 'displayfield',
+                    fieldLabel: 'Total',
+                    action: 'displayFilteredCount'
+                }
+            ]
+        }
+    ],
 });
