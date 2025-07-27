f4eea569a552ee7004696210546c4373821e550d
Benjamin ALLEMAND <benallemand@gmail.com>
2025-07-27 19:11:20 +0200

    footer

---
+++ pages/components/footer/Main.js
@@ -0,0 +1,18 @@
+export default {
+    template: `
+      <div class="bg-base-200 shadow-sm flex justify-center justify-items-center">
+        <div class="mr-2"><i class="fas fa-copyright mr-2"/>2025 - UFOLEP 13 SECTION VOLLEY</div>
+        <div>Retrouvez nous aussi sur notre <a
+            class="link" href="https://www.facebook.com/ufolep13volley/">page Facebook</a> !
+        </div>
+      </div>
+    `,
+    data() {
+        return {
+        };
+    },
+    methods: {
+    },
+    created() {
+    },
+};
