1c16439f22212cb9ba8d91beb01118ce683f3d9b
Benjamin ALLEMAND <benallemand@gmail.com>
2025-10-05 11:55:17 +0200

    Delete .idea directory

--- .idea/dataSources/2ba3cebe-98b2-469f-a1db-0d9e897f2edf/_staging_/schema/ufolep_13volley/routine/SPLIT_STRING.(varchar(255),varchar(12),int(11)).sql
+++
@@ -1,8 +0,0 @@
-SET NAMES UTF8;
-
-DROP FUNCTION IF EXISTS SPLIT_STRING;
-CREATE FUNCTION SPLIT_STRING(str VARCHAR(255), delim VARCHAR(12), pos INT)
-  RETURNS VARCHAR(255)
-  RETURN REPLACE(SUBSTRING(SUBSTRING_INDEX(str, delim, pos),
-                           LENGTH(SUBSTRING_INDEX(str, delim, pos - 1)) + 1),
-                 delim, '');
