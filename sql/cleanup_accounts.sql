869f85973e197c985028e6553095e73379f58608
Benjamin ALLEMAND <benallemand@gmail.com>
2025-10-08 16:53:07 +0200

    cleanup_accounts.sql: suppression des liens vers les équipes de la compétition sélectionnée

--- sql/cleanup_accounts.sql
+++ sql/cleanup_accounts.sql
@@ -1,15 +1,8 @@
 DELETE
-FROM comptes_acces
-WHERE id IN (SELECT user_id
-             FROM users_profiles
-             WHERE profile_id IN (SELECT id
-                                  FROM profiles
-                                  WHERE name IN ('RESPONSABLE_EQUIPE')))
-  AND id IN (SELECT user_id
-             FROM users_teams
-             WHERE team_id IN (SELECT id_equipe
-                               FROM equipes
-                               WHERE code_competition IN (SELECT code_competition
-                                                          FROM competitions
-                                                          WHERE id = ?
-                                                            AND code_competition = id_compet_maitre)))
+FROM users_teams
+WHERE team_id IN (SELECT id_equipe
+                  FROM equipes
+                  WHERE code_competition IN (SELECT code_competition
+                                             FROM competitions
+                                             WHERE id = ?
+                                               AND code_competition = id_compet_maitre))
