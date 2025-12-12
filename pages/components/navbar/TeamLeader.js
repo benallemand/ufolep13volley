ed09f8027509df864db08b39dec6494b90092435
Benjamin ALLEMAND <benallemand@gmail.com>
2025-12-12 22:02:06 +0100

    fix menu responsable

--- pages/components/navbar/TeamLeader.js
+++ pages/components/navbar/TeamLeader.js
@@ -6,22 +6,17 @@ export default {
             <img alt="Ufolep" src="../images/svg/logo-ufolep-vectorizer-no-background.svg" style="max-height:150px;">
           </a>
         </div>
-        <div class="navbar bg-base-100 shadow-sm">
-          <div class="navbar-start">
-            <a href="/">
-              <div tabindex="0" role="button" class="btn btn-ghost">
-                <span><i class="mr-2 fas fa-arrow-left"></i>retour</span>
-              </div>
+        <div class="navbar bg-base-100 shadow-sm flex flex-wrap justify-center gap-2">
+            <a href="/" class="btn btn-ghost">
+              <span><i class="mr-2 fas fa-arrow-left"></i>retour</span>
             </a>
-          </div>
-          <div class="navbar-center flex gap-2">
             <div class="dropdown">
               <div tabindex="0" role="button" class="btn btn-ghost">
                 <span><i class="mr-2 fas fa-user"></i>mon équipe: {{ currentTeam.nom_equipe }}<i class="ml-1 fas fa-chevron-down"/></span>
               </div>
               <ul
                   tabindex="0"
-                  class="menu menu-sm dropdown-content bg-base-100 rounded-box z-50 w-52 p-2 shadow">
+                  class="dropdown-content menu bg-base-100 rounded-box z-50 mt-3 w-52 p-2 shadow">
                 <li v-for="team in teams" :key="team.id_equipe">
                   <a @click="switchTeam(team.id_equipe)">{{ team.nom_equipe }}</a>
                 </li>
@@ -33,7 +28,7 @@ export default {
               </div>
               <ul
                   tabindex="0"
-                  class="menu menu-sm dropdown-content bg-base-100 rounded-box z-50 w-52 p-2 shadow">
+                  class="dropdown-content menu bg-base-100 rounded-box z-50 mt-3 w-52 p-2 shadow">
                 <li>
                   <router-link to="/players"><span><i class="mr-2 fas fa-user"></i>effectif</span></router-link>
                 </li>
@@ -48,7 +43,7 @@ export default {
               </div>
               <ul
                   tabindex="0"
-                  class="menu menu-sm dropdown-content bg-base-100 rounded-box z-50 w-52 p-2 shadow">
+                  class="dropdown-content menu bg-base-100 rounded-box z-50 mt-3 w-52 p-2 shadow">
                 <li><a href="/rest/action.php/team/download_calendar"><span><i class="mr-2 fas fa-download"></i>calendrier</span></a>
                 </li>
                 <li>
@@ -65,7 +60,7 @@ export default {
               </div>
               <ul
                   tabindex="0"
-                  class="menu menu-sm dropdown-content bg-base-100 rounded-box z-50 w-52 p-2 shadow">
+                  class="dropdown-content menu bg-base-100 rounded-box z-50 mt-3 w-52 p-2 shadow">
                 <li>
                   <router-link to="/team_matchs"><span><i class="fas fa-volleyball mr-2"></i>
                     equipe</span></router-link>
@@ -76,19 +71,12 @@ export default {
                 </li>
               </ul>
             </div>
-            <router-link to="/preferences">
-              <div tabindex="0" role="button" class="btn btn-ghost">
-                <span><i class="mr-2 fas fa-gear"></i>préférences</span>
-              </div>
+            <router-link to="/preferences" class="btn btn-ghost">
+              <span><i class="mr-2 fas fa-gear"></i>préférences</span>
             </router-link>
-          </div>
-          <div class="navbar-end">
-            <a href="/rest/action.php/usermanager/logout">
-              <div tabindex="0" role="button" class="btn btn-error">
-                <span><i class="mr-2 fas fa-right-from-bracket"/>déconnexion</span>
-              </div>
+            <a href="/rest/action.php/usermanager/logout" class="btn btn-error">
+              <span><i class="mr-2 fas fa-right-from-bracket"/>déconnexion</span>
             </a>
-          </div>
         </div>
       </div>`,
     data() {
