32d402f1ef21c7a47735deef52e51ab5c1ed559f
Benjamin ALLEMAND <benallemand@gmail.com>
2025-12-14 12:21:45 +0100

    suppression de l'ancien site en angular

--- new_site/pages/footer.php
+++
@@ -1,7 +0,0 @@
-<footer class="footer">
-    <div class="container">
-        <i class="glyphicon glyphicon-copyright-mark" aria-hidden="true"></i> <?php echo date('Y'); ?> - UFOLEP 13
-        SECTION VOLLEY - Retrouvez nous aussi sur notre <a href="https://www.facebook.com/ufolep13volley/">page
-            Facebook</a> !
-    </div>
-</footer>
