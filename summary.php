2147a4de34e6147f28fa558baa284b2d70972f3e
Benjamin ALLEMAND <benallemand@gmail.com>
2026-01-03 08:20:18 +0100

    ajout de liens sur le summary du match

--- summary.php
+++ summary.php
@@ -4,7 +4,16 @@
 </div>
 <div class="flex items-center justify-between mb-4">
     <span class="inline-flex items-center rounded-md bg-blue-50 px-2 py-1 text-xs font-medium text-blue-700 ring-1 ring-inset ring-blue-700/10">{{matchData.libelle_competition}}</span>
-    <span class="inline-flex items-center rounded-md bg-blue-50 px-2 py-1 text-xs font-medium text-blue-700 ring-1 ring-inset ring-blue-700/10">Division {{matchData.division}} Journée {{matchData.numero_journee}}</span>
+    <span class="inline-flex items-center rounded-md bg-blue-50 px-2 py-1 text-xs font-medium text-blue-700 ring-1 ring-inset ring-blue-700/10">
+        <span>
+            <a :href="'/pages/home.html#/divisions/' + matchData.code_competition + '/' + matchData.division"
+               class="link"
+               target="_blank">
+                Division {{matchData.division}}
+            </a>
+        </span>
+        <span v-if="matchData.numero_journee">Journée {{matchData.numero_journee}}</span>
+    </span>
 </div>
 <div class="flex items-center justify-center mb-4">
     <span class="inline-flex items-center rounded-md bg-blue-50 px-2 py-1 text-2xl font-medium text-blue-700 ring-1 ring-inset ring-blue-700/10">{{matchData.code_match}}</span>
@@ -15,10 +24,7 @@
 </div>
 <div class="flex items-center justify-between mb-4">
     <span class="inline-flex items-center rounded-md bg-blue-50 px-2 py-1 text-xs font-medium text-blue-700 ring-1 ring-inset ring-blue-700/10">
-            <a :href="'/pages/home.html#/teams/' + matchData.id_equipe_dom"
-               class="link">
                 {{matchData.gymnasium}}
-            </a>
     </span>
     <span class="inline-flex items-center rounded-md bg-blue-50 px-2 py-1 text-xs font-medium text-blue-700 ring-1 ring-inset ring-blue-700/10">{{matchData.date_reception}} {{matchData.heure_reception}}</span>
 </div>
@@ -26,8 +32,18 @@
 <div class="border border-4">
     <div class="flex flex-row mb-4">
         <span class="basis-1/3"></span>
-        <span class="basis-1/3">{{ matchData.equipe_dom }}</span>
-        <span class="basis-1/3">{{ matchData.equipe_ext }}</span>
+        <span class="basis-1/3"><a :href="'/pages/home.html#/teams/' + matchData.id_equipe_dom"
+                                   class="link"
+                                   target="_blank">
+                {{matchData.equipe_dom}}
+            </a>
+        </span>
+        <span class="basis-1/3"><a :href="'/pages/home.html#/teams/' + matchData.id_equipe_ext"
+                                   class="link"
+                                   target="_blank">
+                {{matchData.equipe_ext}}
+            </a>
+        </span>
     </div>
     <div class="flex flex-row mb-4">
         <span class="basis-1/3">Fiche équipes</span>
